8971d211a8c7ad2d7f45841e45ba7a77a732b2c6
AlexARNcode <alexarnout@gmail.com>
2022-08-06 15:42:34 +0200

    Add a registration system

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,6 +40,9 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            $user->setFirstName($form->get('firstName')->getData());
+            $user->setLastName ($form->get('lastName')->getData());
+
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -53,7 +56,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('api_doc');
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -78,6 +81,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('home');
     }
 }
